2f630a5c7c75a9588805d9d04e0a4e48c1f0958b
jundi1232 <jundinasrullah32@gmail.com>
2022-05-22 23:01:31 +0700

    menambah class baru

---
+++ app/src/main/java/com/jn/capstoneproject/d_jahit/Constanta.kt
@@ -0,0 +1,9 @@
+package com.jn.capstoneproject.d_jahit
+
+import android.Manifest
+
+object Constanta {
+    const val CAMERA_X_RESULT = 200
+    val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    const val REQUEST_CODE_PERMISSIONS = 10
+}
