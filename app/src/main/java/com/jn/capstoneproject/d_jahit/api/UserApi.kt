6e8bdbbc6bda387f6cf0fe364d4f3f6791984495
jundi1232 <jundinasrullah32@gmail.com>
2022-06-07 22:31:36 +0700

    edit APi user

--- app/src/main/java/com/jn/capstoneproject/d_jahit/api/UserApi.kt
+++ app/src/main/java/com/jn/capstoneproject/d_jahit/api/UserApi.kt
@@ -1,30 +1,55 @@
 package com.jn.capstoneproject.d_jahit.api
 
+import com.jn.capstoneproject.d_jahit.model.dataresponse.PutUser
+import com.jn.capstoneproject.d_jahit.model.dataresponse.StatusLoginResponse
+import com.jn.capstoneproject.d_jahit.model.dataresponse.StatusResponse
 import com.jn.capstoneproject.d_jahit.model.dataresponse.UserResponse
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.*
 
 interface UserApi {
 
     @FormUrlEncoded
-    @POST("addUser")
-    suspend fun login(
-//        @Field("fullName") id: String,
-//        @Field("username") password: String,
-//        @Field("password") email: String,
-//        @Field("gender") password: String
-    ): Response<UserResponse>
+    @POST("user")
+    fun addUser(
+        @Field("fullName") Name: String,
+        @Field("email") email: String,
+        @Field("password") password: String,
+    ): Call<StatusResponse>
 
-    @GET("getAllUser/")
-    suspend fun getAllSeller(): Response<UserResponse>
+    @GET("user")
+    fun getAllUser(): Call<PutUser>
 
-    @GET("getUserById")
-    suspend fun getSellerById(): Response<UserResponse>
+    @GET("user/{id}")
+    fun getUserById(
+        @Path("id")
+        id: String
+    ): Call<UserResponse>
 
-    @PUT("editUserById")
-    suspend fun editSellerById(): Response<UserResponse>
 
-    @DELETE("deleteUserIDd")
-    suspend fun deleteSellerByIDd(): Response<UserResponse>
+    @FormUrlEncoded
+    @PUT("user/{id}")
+    fun editUserById(
+        @Path("id") id: String,
+        @Field("fullName") Name: String,
+        @Field("username") username: String,
+        @Field("gender") gender: String,
+        @Field("dateOfBirth") dateOfBirth: String,
+        @Field("phoneNumber") phoneNumber: String,
+        @Field("email") email: String,
+    ): Call<UserResponse>
+
+    @DELETE("user/{id}")
+    fun deleteUserById(
+        @Path("id")
+        id: String
+    ): Call<UserResponse>
+
+    @GET("user/auth/{username}/{password}")
+    fun loginUser(
+        @Path("username") username: String,
+        @Path("password") password: String
+    ): Call<StatusLoginResponse>
 
 }
--- app/src/main/java/com/jn/capstoneproject/d_jahit/model/dataresponse/UserResponse.kt
+++ app/src/main/java/com/jn/capstoneproject/d_jahit/model/dataresponse/UserResponse.kt
@@ -1,36 +1,56 @@
 package com.jn.capstoneproject.d_jahit.model.dataresponse
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class UserResponse(
-    @field:SerializedName("id")
-    val id: String,
 
-    @field:SerializedName("fullname")
-    val fullname: String,
+	@field:SerializedName("password")
+	val password: String? = null,
 
-    @field:SerializedName("username")
-    val username: String,
+	@field:SerializedName("phoneNumber")
+	val phoneNumber: String? = null,
 
-    @field:SerializedName("password")
-    val password: String,
+	@field:SerializedName("photoProfile")
+	val photoProfile: String? = null,
 
-    @field:SerializedName("gender")
-    val gender: String,
+	@field:SerializedName("gender")
+	val gender: String? = null,
 
-    @field:SerializedName("dateOfBirth")
-    val dateOfBirth: String,
+	@field:SerializedName("insertedAt")
+	val insertedAt: String? = null,
 
-    @field:SerializedName("phoneNumber")
-    val phoneNumber: String,
+	@field:SerializedName("latitude")
+	val latitude: String? = null,
 
-    @field:SerializedName("email")
-    val email: String,
+	@field:SerializedName("longtitude")
+	val longtitude: String? = null,
 
-    @field:SerializedName("photoProfile")
-    val photoProfile: String,
+	@field:SerializedName("fullName")
+	val fullName: String? = null,
 
-    @field:SerializedName("insertedAt")
-    val insertedAt: String,
+	@field:SerializedName("dateOfBirth")
+	val dateOfBirth: String? = null,
 
-    )
+	@field:SerializedName("id")
+	val id: String,
+
+	@field:SerializedName("email")
+	val email: String? = null,
+
+	@field:SerializedName("username")
+	val username: String? = null
+):Parcelable
+
+data class PutUser(
+	@field:SerializedName("putuserresponse")
+	val putUserResponse : List<UserResponse>,
+
+	@field:SerializedName("message")
+	val message: String,
+
+	@field:SerializedName("status")
+	val status: String
+)
