61c85a03fc216fe0d452687d759c7b356ce0f3ca
jundi1232 <jundinasrullah32@gmail.com>
2022-06-07 22:34:44 +0700

    add HomeViewModel.kt, ProfileViewModel && CreateSellerViewModel

---
+++ app/src/main/java/com/jn/capstoneproject/d_jahit/ViewModelFactory.kt
@@ -0,0 +1,37 @@
+package com.jn.capstoneproject.d_jahit
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.jn.capstoneproject.d_jahit.model.Repository
+import com.jn.capstoneproject.d_jahit.viewmodel.*
+import java.lang.IllegalArgumentException
+
+class ViewModelFactory constructor(private val context: Context) :
+    ViewModelProvider.NewInstanceFactory() {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(RegisterViewModel::class.java) -> {
+                RegisterViewModel(Injection.provideUserRepository(context)) as T
+            }
+            modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
+                LoginViewModel(Injection.provideUserRepository(context)) as T
+            }
+            modelClass.isAssignableFrom(ProfileViewModel::class.java) -> {
+                ProfileViewModel(Injection.provideUserRepository(context)) as T
+            }
+            modelClass.isAssignableFrom(CreateSellerViewmodel::class.java) -> {
+                CreateSellerViewmodel(Injection.provideUserRepository(context)) as T
+            }
+            modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
+                HomeViewModel(Injection.provideUserRepository(context)) as T
+            }
+
+
+            else -> throw IllegalArgumentException("Unkown ViewModel Class ${modelClass.name}")
+
+        }
+    }
+
+}
